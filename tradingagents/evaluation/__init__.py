48a52ea75eea9aaf03c7e119e6178d4f0d323ba1
wyh <102863847+wyh22@users.noreply.github.com>
2026-09-06 15:51:29 +0800

    feat: export routing grounding and baseline evaluators

--- tradingagents/evaluation/__init__.py
+++ tradingagents/evaluation/__init__.py
@@ -14,7 +14,9 @@ from .evaluators import (
     TrajectoryEvaluator,
 )
 from .runner import run_evaluation, run_single_eval_case
-from .trajectory import (
+from .baseline import SystemEvalSummary, compare_systems
+from .grounding import GroundingEvalResult, evaluate_claim_grounding
+from .routing import (\n    RoutingEvalCase,\n    RoutingEvalResult,\n    RoutingRecord,\n    evaluate_route,\n    load_routing_eval,\n    run_routing_eval,\n    summarize_routing,\n)\nfrom .trajectory import (
     ToolCallRecord,
     TrajectoryNode,
     TrajectoryRecord,
@@ -41,4 +43,15 @@ __all__ = [
     "run_evaluation",
     "run_single_eval_case",
     "score_trajectory",
+    "SystemEvalSummary",
+    "compare_systems",
+    "GroundingEvalResult",
+    "evaluate_claim_grounding",
+    "RoutingEvalCase",
+    "RoutingEvalResult",
+    "RoutingRecord",
+    "evaluate_route",
+    "load_routing_eval",
+    "run_routing_eval",
+    "summarize_routing",
 ]
